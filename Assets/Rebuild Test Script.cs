320bb18532afeb6a29db535c5ba3026922e2f33c
Batuhan <batuhan.avci@outlook.de>
2026-02-25 14:50:20 +0100

    Added Test SCript

---
+++ Assets/Rebuild Test Script.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class RebuildTestScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Miep");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
